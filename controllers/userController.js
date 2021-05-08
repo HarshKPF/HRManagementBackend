06d93966bbe6d67faf098c7e5ab31e7c27101185
Ritika <meena@purplefront.com>
2021-05-08 18:26:04 +0530

    Sample PUT, DELETE APIs

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,8 +3,8 @@ const userController = {
     getUsersList: async(req, res) => {
         try {
             //Sample GET API
-            const users = await db.query('SELECT first_name, last_name FROM users ORDER BY id ASC LIMIT 5');
-            const message = "I am Sample GET API Response";
+            const users = await db.query('SELECT id, first_name, last_name FROM users ORDER BY id ASC LIMIT 5');
+            const message = "Data Retrieved Successfully";
                 
             return res.status(200).send({//OK
                 status: 200,
@@ -39,7 +39,7 @@ const userController = {
 
             return res.status(200).send({//OK
                 status: 200,
-                message: "I am Sample POST API Response",
+                message: "User Created Successfully",
                 data: {
                     first_name,
                     last_name
@@ -51,8 +51,54 @@ const userController = {
                 message: "Something Went Wrong"
             });
         }
+    },
+    updateUserDetails: async(req, res) => {
+        try {
+            //Sample PUT API
+            //Get Payload from request body
+            const {id} = req.params;
+            const {first_name, last_name} = req.body;
+            await db.query(
+                "UPDATE users SET first_name = $1, last_name = $2 WHERE id = $3",
+                [first_name, last_name, id]
+            );
+
+            return res.status(200).send({//OK
+                status: 200,
+                message: "User Details Updated Successfully",
+                data: {
+                    first_name,
+                    last_name
+                }
+            });
+        } catch (error) {
+            return res.status(500).send({//Internal Error
+                status: 500,
+                message: "Something Went Wrong"
+            });
+        }
+    },
+    deleteUser: async(req, res) => {
+        try {
+            //Sample DELETE API
+            //Get Payload from request body
+            const {id} = req.params;
+            await db.query(
+                "DELETE FROM users WHERE id = $1",
+                [id]
+            );
+
+            return res.status(200).send({//OK
+                status: 200,
+                message: "User Details Deleted Successfully"
+            });
+        } catch (error) {
+            return res.status(500).send({//Internal Error
+                status: 500,
+                message: "Something Went Wrong"
+            });
+        }
     }
-    
 }
 
 module.exports = userController;
